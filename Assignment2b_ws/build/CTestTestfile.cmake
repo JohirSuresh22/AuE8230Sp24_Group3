a98a425b0de7e8d39edc3bef3a718667ad57208c
JohirSuresh22 <johirsuresh1998@gmail.com>
2024-02-08 16:13:37 -0500

    Pushing the initial setup

---
+++ Assignment2b_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/AuE8230Sp24_Group3/Assignment2b_ws/src
+# Build directory: /home/ubuntu/AuE8230Sp24_Group3/Assignment2b_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
