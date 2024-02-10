210563c939b5ce43c664037029bb9a1c5e4ab766
JohirSuresh22 <johirsuresh1998@gmail.com>
2024-02-10 15:28:15 -0500

    This is a new folder on PC

---
+++ a2b_ws/build/assign2b/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/AuE8230Sp24_Group3/a2b_ws/src/assign2b
+# Build directory: /home/ubuntu/AuE8230Sp24_Group3/a2b_ws/build/assign2b
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ a2b_ws/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/ubuntu/AuE8230Sp24_Group3/a2b_ws/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
