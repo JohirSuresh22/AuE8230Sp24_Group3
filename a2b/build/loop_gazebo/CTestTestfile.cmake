aad52af5aff92677ebd6a5165907fef05271cb72
Jagadeesh Madinni <jmadinn@clemson.edu>
2024-02-10 22:28:03 -0500

    Resolved launch file errors

---
+++ a2b/build/loop_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/beetrootsalad/AuE8230Sp24_Group3/a2b/src/loop_gazebo
+# Build directory: /home/beetrootsalad/AuE8230Sp24_Group3/a2b/build/loop_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
